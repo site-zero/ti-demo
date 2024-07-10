d0d81697d167a50dd6ddfafd2ea50bab01d9ffc3
Peter Zhang <zozohtnt@gmail.com>
2024-07-11 02:10:12 +0800

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vite";
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  optimizeDeps: {
+    exclude: ['vitest'],
+  },
   build: {
     // 单位是 KB
-    chunkSizeWarningLimit: 2000
-  }
+    chunkSizeWarningLimit: 2000,
+  },
 });
