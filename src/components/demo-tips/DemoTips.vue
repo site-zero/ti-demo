5212a6753584b1a4a9dcf6ac21ad0f2beba4658d
Peter Zhang <zozohtnt@gmail.com>
2025-02-08 01:02:00 +0800

    ...

--- src/components/demo-tips/DemoTips.vue
+++ src/components/demo-tips/DemoTips.vue
@@ -11,27 +11,62 @@
   });
   _tips?.addTip({
     selector: 'th:first-child',
-    content: 'hahahah',
+    content:
+      'Customizing TiTextSnippet in Vue 3 with Vite using JSX brings new flexibility.',
+    dockMode: 'H-left',
+    width: '120px',
+    type: 'danger',
+  });
+  _tips?.addTip({
+    selector: 'th:last-child',
+    content:
+      'Customizing TiTextSnippet in Vue 3 with Vite using JSX brings new flexibility.',
+    dockMode: 'H-right',
+    width: '120px',
+    type: 'warn',
+  });
+  _tips?.addTip({
+    selector: 'td:first-child',
+    content:
+      'Customizing TiTextSnippet in Vue 3 with Vite using JSX brings new flexibility.',
+    dockMode: 'V-top',
+    width: '120px',
+    type: 'number',
+  });
+  _tips?.addTip({
+    selector: 'td:last-child',
+    content:
+      'Customizing TiTextSnippet in Vue 3 with Vite using JSX brings new flexibility.',
+    dockMode: 'V-bottom',
+    width: '120px',
+    type: 'text',
   });
 </script>
 <template>
   <div class="demo-tips" ref="main">
-    <section>
-      <a href="#A" data-tip-id="AAA" data-tip-content="Customizing TiTextSnippet in Vue 3 with Vite using JSX brings new flexibility.">AAA</a>
+    <h3>this is some tips</h3>
+    <section class="as-links">
+      <a
+        href="#"
+        data-tip-id="AAA"
+        data-tip-content="Customizing TiTextSnippet in Vue 3 with Vite using JSX brings new flexibility."
+        data-tip-padding="b">
+        Hover to show tip
+      </a>
     </section>
     <hr />
     <section>
       <table border="1">
         <thead>
           <tr>
-            <th>Title A</th>
-            <th>Title B</th>
+            <th>H-left</th>
+            <th>H-right</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>Cell A</td>
-            <td>Cell B</td>
+            <td>V-top</td>
+            <td>V-bottom</td>
           </tr>
         </tbody>
       </table>
@@ -40,4 +75,13 @@
 </template>
 <style scoped lang="scss">
   @use '@site0/tijs/sass/_all.scss' as *;
+  section.as-links {
+    @include flex-align;
+  }
+  table {
+    td,
+    th {
+      padding: 0.5em 2em;
+    }
+  }
 </style>
